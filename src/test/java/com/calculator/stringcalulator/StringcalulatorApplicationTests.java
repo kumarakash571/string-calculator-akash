c7c0236e22edeaf096ea759d94d82a5a4621df33
kumarakash571 <akashkumarbxr571@gmail.com>
2025-09-25 12:03:49 -0700

    TDD: implement add(\"\") -> 0"

--- src/test/java/com/calculator/stringcalulator/StringcalulatorApplicationTests.java
+++ src/test/java/com/calculator/stringcalulator/StringcalulatorApplicationTests.java
@@ -1,5 +1,6 @@
 package com.calculator.stringcalulator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,12 +8,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StringcalulatorApplicationTests {
 
-	private StringcalulatorApplication calc;
+	private StringCalculator calc;
     
 	@BeforeEach
 	void setUp(){
-		calc = new StringcalulatorApplication();
+		calc = new StringCalculator();
 	}
+
+	@Test
+	void addEmptyString(){
+		assertEquals(0, calc.add(""));
+	}
+
+	@Test
+    void add_multipleNumbers() {
+       assertEquals(15, calc.add("1,2,3,4,5"));
+  }
 
 	@Test
 	void contextLoads() {
