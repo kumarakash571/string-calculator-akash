a4c06d4ff2f299fc99fea53570c7db8a52abebb9
kumarakash571 <akashkumarbxr571@gmail.com>
2025-09-25 10:34:07 -0700

    first change

---
+++ src/main/java/com/calculator/stringcalulator/StringcalulatorApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.stringcalulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringcalulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringcalulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculator/stringcalulator/StringcalulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.stringcalulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringcalulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
